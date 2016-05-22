d9ca52c1f3fcd061ce45a60b267998017cb0d113
Alexander Ewetumo <trinoxf@gmail.com>
2016-05-22 18:55:10 +0100

    updating mongodb middleware

--- web/middleware/db.go
+++ web/middleware/db.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/ardanlabs/kit/cfg"
 	"github.com/influx6/faux/context"
 	"github.com/influx6/faux/db/mongo"
 	"github.com/influx6/faux/web/app"
@@ -33,18 +34,60 @@ func (l eventlog) Error(context interface{}, name string, err error, message str
 
 //==============================================================================
 
-// MongoDBKey defines the key used to retrieve the configuration for creating a
-// mongodb session in a context.
-var MongoDBKey = "MONGO_DB"
+// MongoSessionKey defines the key which locks the mongo session provided into
+// the passed context using the mongo middleware.
+var MongoSessionKey = "MONGO_DB_SESSION"
+
+// MongoDatabaseKey defines the key which locks the mongo database to be used
+// directly from the giving mongo configuration. It lets you access the proper
+// db without knowledge of the name by using the config DB attribute.
+var MongoDatabaseKey = "MONGO_DB_DATABASE"
 
 // MongoDB returns the middleware which creates a mongo database session for
 // the giving context.
 func MongoDB(c mongo.Config, l Log) app.Middleware {
-	md := &mongo.Mongnod{C: c, EventLog: l}
+	if l == nil {
+		l = events
+	}
+
+	md := &mongo.Mongnod{C: c, Log: l}
 
 	return func(h app.Handler) app.Handler {
 		return func(ctx context.Context, w *app.ResponseRequest) error {
-			return h(ctx, w)
+			db, ses, err := md.New(nil)
+			if err != nil {
+				l.Error("app.Middleware", "MongoDB", err, "Completed")
+				return err
+			}
+
+			// End the session once this is done.
+			defer ses.Close()
+
+			if err := h(ctx.WithValue(MongoSessionKey, ses).WithValue(MongoDatabaseKey, db), w); err != nil {
+				l.Error("app.Middleware", "MongoDB", err, "Completed")
+				return err
+			}
+
+			return nil
 		}
 	}
+}
+
+// MongoDBEnv returns a middleware which loads its configuration from the
+// environment variables of the host system.
+func MongoDBEnv(configName string, l Log) app.Middleware {
+	if l == nil {
+		l = events
+	}
+
+	// Initialize the configuration system to retrieve environment variavles.
+	cfg.Init(cfg.EnvProvider{Namespace: configName})
+
+	return MongoDB(mongo.Config{
+		Host:     cfg.MustString("MONGO_HOST"),
+		AuthDB:   cfg.MustString("MONGO_AUTHDB"),
+		DB:       cfg.MustString("MONGO_DB"),
+		User:     cfg.MustString("MONGO_USER"),
+		Password: cfg.MustString("MONGO_PASS"),
+	}, l)
 }
