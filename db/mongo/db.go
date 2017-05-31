82ba47863bb5b01f0408e139739e08d06a058d9f
Alexander Ewetumo <trinoxf@gmail.com>
2017-05-31 11:08:44 +0100

    Add db update

--- db/mongo/db.go
+++ db/mongo/db.go
@@ -8,22 +8,6 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
-//==============================================================================
-
-var mstore struct {
-	ml   sync.RWMutex
-	list map[string]*mgo.Session
-}
-
-// masterListLock provides a mutex for controlling access to the masterList.
-var masterListLock sync.RWMutex
-
-// masterList contains a set of session lists of connections that have been
-// created
-var masterList = make(map[string]*mgo.Session)
-
-//==============================================================================
-
 // Config provides configuration for connecting to a db.
 type Config struct {
 	Host     string
@@ -33,63 +17,61 @@ type Config struct {
 	Password string
 }
 
-//==============================================================================
-
-// EventLog defines event logger that allows us to record events for a specific
-// action that occured.
-type EventLog interface {
-	Dev(context interface{}, name string, message string, data ...interface{})
-	User(context interface{}, name string, message string, data ...interface{})
-	Error(context interface{}, name string, err error, message string, data ...interface{})
+// MongoServer defines a mongo connection manager that builds 
+// allows usage of a giving configuration to generate new mongo 
+// sessions and database instances.
+type MongoServer struct {
+	Config
+	sl sync.Mutex
+	sessions map[string*mgo.Session
 }
 
-//==============================================================================
+// New returns a new instance of a MongoServer.
+func New(config Config) *MongoServer {
+	var mn MongoServer
+	mn.Config = config
+	mn.sessions = make(map[string]*mgo.Session)
 
-// Mongnod defines a mongo connection manager that builds off a mongo instance.
-type Mongnod struct {
-	C   Config
-	Log EventLog
+	return &mn
 }
+// New returns a new session and database from the giving configuration.
+func (m *Mongnod) NewSession() (*mgo.Database, *mgo.Session, error) {
+	key := m.Config.Host + ":" + m.Config.DB
 
-// New connects and initializes the master session for the mongo list.
-func (m *Mongnod) New(context interface{}) (*mgo.Database, *mgo.Session, error) {
-	key := m.C.Host + ":" + m.C.DB
-
-	masterListLock.Lock()
-	ms, ok := masterList[key]
-	masterListLock.Unlock()
+	m.sl.Lock()	
+	ms, ok := m.sessions[key]
+	m.sl.Unlock()	
 
 	if ok {
 		ses := ms.Copy()
-		return ses.DB(m.C.DB), ses, nil
+		return ses.DB(m.Config.DB), ses, nil
 	}
 
 	// If not found, then attemp to connect and add to session master list.
 	// We need this object to establish a session to our MongoDB.
 	info := mgo.DialInfo{
-		Addrs:    []string{m.C.Host},
+		Addrs:    []string{m.Config.Host},
 		Timeout:  60 * time.Second,
-		Database: m.C.AuthDB,
-		Username: m.C.User,
-		Password: m.C.Password,
+		Database: m.Config.AuthDB,
+		Username: m.Config.User,
+		Password: m.Config.Password,
 	}
 
 	// Create a session which maintains a pool of socket connections
 	// to our MongoDB.
 	ses, err := mgo.DialWithInfo(&info)
 	if err != nil {
-		m.Log.Error(context, "New", err, "Completed")
 		return nil, nil, err
 	}
 
 	ses.SetMode(mgo.Monotonic, true)
 
 	// Add to master list.
-	masterListLock.Lock()
-	masterList[key] = ses.Copy()
-	masterListLock.Unlock()
+	m.sl.Lock()	
+	m.sessions[key] = ses.Copy()
+	m.sl.Unlock()	
 
-	return ses.DB(m.C.DB), ses, nil
+	return ses.DB(m.Config.DB), ses, nil
 }
 
 //==========================================================================================
