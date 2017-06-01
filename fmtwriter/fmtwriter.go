b80deca8bf3e4c0bc3a2888c300a739a8e1bd143
Alexander Ewetumo <trinoxf@gmail.com>
2017-06-01 11:33:03 +0100

    Add option for goimport

--- fmtwriter/fmtwriter.go
+++ fmtwriter/fmtwriter.go
@@ -13,18 +13,25 @@ import (
 // against go fmt and returns the result.
 type WriterTo struct {
 	io.WriterTo
+	goimport bool
 }
 
 // New returns a new instance of FmtWriterTo.
-func New(wt io.WriterTo) *WriterTo {
-	return &WriterTo{WriterTo: wt}
+func New(wt io.WriterTo, useGoImports bool) *WriterTo {
+	return &WriterTo{WriterTo: wt, goimport: useGoImports}
 }
 
 // WriteTo writes the content of the source after running against gofmt to the
 // provider writer.
 func (fm WriterTo) WriteTo(w io.Writer) (int64, error) {
+	cmdName := "gofmt"
+
+	if fm.goimport {
+		cmdName = "goimports"
+	}
+
 	cmd := process.Command{
-		Name:  "gofmt",
+		Name:  cmdName,
 		Level: process.RedAlert,
 	}
 
