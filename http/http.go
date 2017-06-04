ecfba040aea3315b03ea0e39971f0900bde4e488
Alexander Ewetumo <trinoxf@gmail.com>
2017-06-04 18:14:46 +0100

    Add Params method for request

--- http/http.go
+++ http/http.go
@@ -15,16 +15,16 @@ const (
 
 // Params defines a function to return all parameter values and query values
 // retrieved from the request.
-func Params(r *http.Request, mxsize int64) (context.Context, error) {
-	if mxsize <= 0 {
-		mxsize = maxsize
+func Params(r *http.Request, multipartFormSize int64) (context.Context, error) {
+	if multipartFormSize <= 0 {
+		multipartFormSize = maxsize
 	}
 
 	if err := r.ParseForm(); err != nil {
 		return nil, err
 	}
 
-	if err := r.ParseMultipartForm(mxsize); err != nil {
+	if err := r.ParseMultipartForm(multipartFormSize); err != nil {
 		return nil, err
 	}
 
