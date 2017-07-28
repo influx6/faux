88bbc31f38fcc92b4dbf0ba6445358fa0b20b6b4
Alexander Ewetumo <trinoxf@gmail.com>
2017-07-28 21:32:36 +0100

    Add files pkg

---
+++ files/file.go
@@ -0,0 +1,87 @@
+package files
+
+import (
+	"bytes"
+	"os"
+	"time"
+)
+
+// DodFile defines a standin structure for a giving http.File object.
+type DodFile struct {
+	*bytes.Reader
+	Info os.FileInfo
+	Dirs []os.FileInfo
+}
+
+// New returns a new DodFile instance.
+func New(name string, body []byte, dirs ...os.FileInfo) *DodFile {
+	return &DodFile{
+		Dirs: dirs,
+		Info: &DodInfo{
+			FileName: name,
+			FileSize: int64(len(body)),
+		},
+		Reader: bytes.NewReader(body),
+	}
+}
+
+// Close returns nil always
+func (d DodFile) Close() error {
+	return nil
+}
+
+// Stat returns the FileInfo associated wih the Dodfile.
+func (d DodFile) Stat() (os.FileInfo, error) {
+	return d.Info, nil
+}
+
+// Readdir returns the Dirs field as the list of directories.
+func (d DodFile) Readdir(dm int) ([]os.FileInfo, error) {
+	return d.Dirs, nil
+}
+
+// DodInfo implements the FileInfo for usage as a os.FileInfo.
+type DodInfo struct {
+	FileName string
+	FileSize int64
+}
+
+// Sys returns always nil.
+func (m DodInfo) Sys() interface{} {
+	return nil
+}
+
+// Mode returns the current time.
+func (m DodInfo) Mode() os.FileMode {
+	return 0777
+}
+
+// ModTime returns the current time.
+func (m DodInfo) ModTime() time.Time {
+	return time.Now()
+}
+
+// Size returns the size associated for the structure.
+func (m DodInfo) Size() int64 {
+	return m.FileSize
+}
+
+// IsDir returns always false.
+func (m DodInfo) IsDir() bool {
+	return false
+}
+
+// IsRegular reports whether m describes a regular file.
+func (m DodInfo) IsRegular() bool {
+	return true
+}
+
+// Perm returns the Unix permission bits in m.
+func (m DodInfo) Perm() os.FileMode {
+	return 0700
+}
+
+// Name returns the name associated for the DodInfo
+func (m DodInfo) Name() string {
+	return m.FileName
+}
