bc38f8bdf409fa5872cc6ff81c7c58bea98730c4
Alexander Ewetumo <trinoxf@gmail.com>
2017-11-11 13:28:43 +0100

    Update templateutil FromWith/NewWith methods

--- tmplutil/templateutil.go
+++ tmplutil/templateutil.go
@@ -198,6 +198,84 @@ func (g *Group) Add(name string, data string) *Group {
 	return g
 }
 
+// FromWith acts like New but calls template.Lookup on the retunred template
+// to return the first template object forthe first name in the slice provided.
+func (g *Group) FromWith(fn template.FuncMap, names ...string) (*template.Template, error) {
+	if len(names) == 0 {
+		return nil, errors.New("Require name slice")
+	}
+
+	tml, err := g.NewWith(fn, names...)
+	if err != nil {
+		return nil, err
+	}
+
+	if initialTml := tml.Lookup(names[0]); initialTml != nil {
+		return initialTml, nil
+	}
+
+	return nil, fmt.Errorf("template object for %+q not found", names[0])
+}
+
+// NewWith returns a template from all templates within group if no names
+// are provided. Either arrange names of support template to main template
+// else use template.Lookup method to retrieve template for respective name.
+func (g *Group) NewWith(fn template.FuncMap, names ...string) (*template.Template, error) {
+	g.ml.RLock()
+	defer g.ml.RUnlock()
+
+	var tmpl *template.Template
+
+	if len(names) != 0 {
+		for _, name := range names {
+			data, ok := g.templates[name]
+			if !ok {
+				continue
+			}
+
+			if tmpl != nil {
+				ml, err := tmpl.New(name).Funcs(defaultFuncs).Funcs(fn).Parse(data)
+				if err != nil {
+					return nil, err
+				}
+
+				tmpl = ml
+				continue
+			}
+
+			tml, err := template.New(name).Funcs(defaultFuncs).Funcs(fn).Parse(data)
+			if err != nil {
+				return nil, err
+			}
+
+			tmpl = tml
+		}
+
+		return tmpl, nil
+	}
+
+	for name, data := range g.templates {
+		if tmpl != nil {
+			ml, err := tmpl.New(name).Funcs(defaultFuncs).Funcs(fn).Parse(data)
+			if err != nil {
+				return nil, err
+			}
+
+			tmpl = ml
+			continue
+		}
+
+		tml, err := template.New(name).Funcs(defaultFuncs).Funcs(fn).Parse(data)
+		if err != nil {
+			return nil, err
+		}
+
+		tmpl = tml
+	}
+
+	return tmpl, nil
+}
+
 // From acts like New but calls template.Lookup on the retunred template
 // to return the first template object forthe first name in the slice provided.
 func (g *Group) From(names ...string) (*template.Template, error) {
