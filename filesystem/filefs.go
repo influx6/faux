192eaa523dd703dfcbb12a2cde25cdbce744dceb
Alexander Ewetumo <trinoxf@gmail.com>
2017-12-05 16:10:25 +0100

    Update filesystem with fileportal

--- filesystem/filefs.go
+++ filesystem/filefs.go
@@ -6,15 +6,31 @@ import (
 	"path/filepath"
 )
 
+// FilePortal defines an error which exposes methods to
+// treat a underline store has a file system.
+type FilePortal interface {
+	Remove(string) error
+	RemoveAll() error
+	Save(string, []byte) error
+	Get(string) ([]byte, error)
+	Within(string) (FilePortal, error)
+}
+
 // FileFS implements a simple store for storing and retrieving
 // file from underneath filesystem.
 type FileFS struct {
 	Dir string
 }
 
-// Persist saves giving file into FileFS.Dir, overwriting any same file
+// Within returns a new FilePortal which exists within the current path.
+// It enforces all operations to occur within provided path.
+func (fs FileFS) Within(path string) (FilePortal, error) {
+	return FileFS{Dir: filepath.Join(fs.Dir, path)}, nil
+}
+
+// Save saves giving file into FileFS.Dir, overwriting any same file
 // existing.
-func (fs FileFS) Persist(file string, data []byte) error {
+func (fs FileFS) Save(file string, data []byte) error {
 	targetPath := filepath.Join(fs.Dir, file)
 	targetDir := filepath.Dir(targetPath)
 
@@ -35,8 +51,8 @@ func (fs FileFS) Persist(file string, data []byte) error {
 	return err
 }
 
-// RemoveContents remove FileFS.Dir and contents.
-func (fs FileFS) RemoveContents() error {
+// RemovAll remove FileFS.Dir and contents.
+func (fs FileFS) RemoveAll() error {
 	if err := os.RemoveAll(fs.Dir); err != nil {
 		if perr, ok := err.(*os.PathError); ok && perr.Err == os.ErrNotExist {
 			return nil
@@ -56,8 +72,8 @@ func (fs FileFS) Remove(file string) error {
 	return os.Remove(targetFile)
 }
 
-// Retrieve retrieves giving file path within FileFS.Dir.
-func (fs FileFS) Retrieve(file string) ([]byte, error) {
+// Get retrieves giving file path within FileFS.Dir.
+func (fs FileFS) Get(file string) ([]byte, error) {
 	targetFile := filepath.Join(fs.Dir, file)
 	if _, err := os.Stat(targetFile); err != nil {
 		return nil, err
