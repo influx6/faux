c8b5f9b8035cac9d83babf7c8c42cbb67636b368
Alexander Ewetumo <trinoxf@gmail.com>
2017-12-23 08:30:55 +0100

    Add id for log printing in metrics pkg

---
+++ httputil/server_older_go.go
@@ -0,0 +1,13 @@
+// +build go1.2,go1.3,go1.4,go1.5,go1.6,go1.7
+
+package httputil
+
+import (
+	"context"
+)
+
+// Close closes the underline server.
+// It will forcefully close the server.
+func (s serverItem) Close(ctx context.Context) error {
+	return s.listener.Close()
+}
